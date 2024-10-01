9ff7133003b8427535aad1b11ebd05c991e994cf
anyu017 <A113221017@mail.shu.edu.tw>
2024-10-01 15:37:02 +0800

    113-10-01

---
+++ 1130924/hello/hello/Form1.cs
@@ -0,0 +1,15 @@
+namespace hello
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btndDisplay_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello world");
+        }
+    }
+}
